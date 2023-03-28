a9c95cf1d48983efd30792f01069fe8d00fa9c71
Maya Tabbah <mayatabbah7@gmail.com>
2023-03-28 14:37:37 -0400

    yay

--- lib/main.dart
+++ lib/main.dart
@@ -37,12 +37,13 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
           Text('A VERY AWESOME random idea:'),
-          Text(appState.current.asLowerCase),
+          Text(pair.asLowerCase),
           ElevatedButton(
             onPressed: () {
               appState.getNext();
